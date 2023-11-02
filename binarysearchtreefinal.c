cc638bf3320d9eacf231afe807a818023f62e8bd
Ajith Mani Santhosh <146512478+Ajith-Ambattuparambil@users.noreply.github.com>
2023-11-02 11:47:12 +0530

    Add files via upload

---
+++ binarysearchtreefinal.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+  int key;
+  struct node *left, *right;
+};
+int item,flag=0;
+void search(struct node *root);
+// Create a node
+struct node *newNode(int item) {
+  struct node *temp = (struct node *)malloc(sizeof(struct node));
+  temp->key = item;
+  temp->left = temp->right = NULL;
+  return temp;
+}
+// Inorder Traversal
+void inorder(struct node *root) {
+  if (root != NULL) {
+    // Traverse left
+    inorder(root->left);
+    // Traverse root
+    printf("%d -> ", root->key);
+    // Traverse right
+    inorder(root->right);
+  }
+}
+// Insert a node
+struct node *insert(struct node *node, int key) {
+  // Return a new node if the tree is empty
+  if (node == NULL) return newNode(key);
+  // Traverse to the right place and insert the node
+  if (key < node->key)
+    node->left = insert(node->left, key);
+  else
+    node->right = insert(node->right, key);
+  return node;
+}
+// Find the inorder successor
+struct node *minValueNode(struct node *node) 
+{
+  struct node *current = node;
+  // Find the leftmost leaf
+  while (current && current->left != NULL)
+    current = current->left;
+  return current;
+}
+//Deleting a node
+struct node *deleteNode(struct node *root, int key) {
+  // Return if the tree is empty
+  if (root == NULL) return root;
+  // Find the node to be deleted
+  if (key < root->key)
+    root->left = deleteNode(root->left, key);
+  else if (key > root->key)
+    root->right = deleteNode(root->right, key);
+  else {
+    // If the node is with only one child or no child
+    if (root->left == NULL) {
+      struct node *temp = root->right;
+      free(root);
+      return temp;
+    } else if (root->right == NULL) {
+      struct node *temp = root->left;
+      free(root);
+      return temp;
+    }
+    // If the node has two children
+    struct node *temp = minValueNode(root->right);
+    // Place the inorder successor in position of the node to be deleted
+    root->key = temp->key;
+    // Delete the inorder successor
+    root->right = deleteNode(root->right, temp->key);
+  }
+  return root;
+}
+// Driver code
+int main() {
+  struct node *root = NULL;
+  int i,n,m,d,choice;
+  do
+  {
+  printf("\nEnter your choice:\n1. Insertion\n2. Deletion\n3. Search\n4. Exit\n");
+  scanf("%d",&choice);
+  switch(choice)
+  {
+  case 1:
+  printf("\nEnter the number of elements to be inserted in BST:\n");
+  scanf("%d",&n);
+  printf("Enter the %d elements:\n",n);
+  for(i=0;i<n;i++)
+  {
+  scanf("%d",&m);
+  root = insert(root, m);
+  }
+  printf("\nInorder traversal:\n");
+  inorder(root);
+  break;
+  case 2:
+  printf("\nEnter the value to be deleted:\n");
+  scanf("%d",&d);
+  printf("\nAfter deleting %d\n",d);
+  root = deleteNode(root, d);
+  printf("Inorder traversal:\n");
+  inorder(root);
+  break;
+  case 3: printf("\nEnter the value to be searched \n");
+          scanf("%d",&item);
+          search(root);
+          if(flag==0)
+             printf("Element not found");
+          flag=0;
+  case 4:
+  printf("\nExiting\n");
+  break;
+  }
+  }while(choice!=4);
+  return 0;
+ } 
+void search(struct node *root)
+    {
+       if (root != NULL) 
+       {
+        // Traverse left
+        search(root->left);
+         // Traverse root
+         if(item==root->key)
+         {
+         printf("%d found in binary search tree", root->key);
+         flag=1;
+         }
+         // Traverse right
+         search(root->right);
+               } 
+    }
