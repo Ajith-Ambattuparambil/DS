f36407e1360775673fcdea786753da3965bd4f57
Ajith Mani Santhosh <146512478+Ajith-Ambattuparambil@users.noreply.github.com>
2023-11-30 14:33:00 +0530

    Update kruskal.c

--- kruskal.c
+++ kruskal.c
@@ -1,61 +1,74 @@
 #include<stdio.h>
 #include<stdlib.h>
-int i,j,k,a,b,u,v,n,ne=1;
-int min,mincost=0,cost[9][9],parent[9];
-int find(int);
-int uni(int,int);
-int main()
+#define MAX 10
+int parent[MAX];
+int v,i,j,cost[MAX][MAX],edgecount,count=1,mincost,c,rno,cno,v1,v2;
+int find(int i)
+{ 
+	return parent[i]; 
+} 
+						  
+int uni(int i,int j)
 {
-printf("Kruskal's Algorithm in C\n");
-printf("=================\n");
-printf("\nEnter the no. of vertices:\n");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
-{
- for(j=0;j<=n;j++)
- {
- printf("Enter cost(%d,%d)",i,j);
- scanf("%d",&cost[i][j]);
- if(cost[i][j]==0)
- cost[i][j]=999;
- }}
- printf("The edges of minimum cost spanning tree are:\n");
- while(ne<n)
- {
- for(i=1,min=999;i<=n;i++)
- {
- for(j=1;j<=n;j++)
- { 
- if (cost[i][j]<min)
- {
- min=cost[i][j];
- a=u=i;
- b=v=j;
- }}} 
- u=find(u);
- v=find(v);
- if(uni(u,v))
- {
- printf("%d Edge(%d,%d)=%d\n",ne++,a,b,min);
- mincost=min;
- }
- }
- cost[a][b]=cost[b][a]=999;
- printf("\nMinimum cost=%d\n",mincost);
- return 1;
- }
- int find(int i)
- {
- while(parent[i])
- i=parent[i];
- return i;
- }
- int uni(int i,int j)
- {
- if(i!=j)
- {
- parent[j]=i;
- return 1;
- }
- return 0;
- }
+	if(i!=j)
+	{  
+		parent[j]=i; 
+		return 1; 
+	} 
+	return 0; 
+} 
+	 
+	 
+int main(){  
+	printf("Total no of v :: ");
+	scanf("%d",&v);
+	for(i=1;i<=v;i++)
+		 parent[i]=i;
+	//Get edge weight matrix from user	
+	for(i=1;i<=v;i++)
+	{
+		for(j=i+1;j<=v;j++)
+		{
+			if(i!=j)
+			{
+				printf("enter the cost[%d][%d]||cost[%d][%d]:",i,j,j,i);
+				scanf("%d",&cost[i][j]);
+				cost[j][i]=cost[i][j];
+				if(cost[i][j]==0)
+					{
+					   cost[i][j]=999;
+					   cost[j][i]=999;
+					}
+			}
+		}
+	}
+	
+	edgecount = v-1;
+	
+	while(count <= edgecount)
+	{
+	c=999;
+		for(i=1;i<=v;i++)
+		{
+			for(j=1;j<=v;j++)
+			{
+				if(cost[i][j] < c)
+				{
+						c = cost[i][j];
+						v1 = rno =  i;
+						v2 = cno=  j;
+				}
+			}
+		}   
+		rno  = find(rno);  
+		cno = find(cno);  
+		if(uni(rno,cno))
+		{  
+			printf("\nEdge %d is (%d -> %d) with cost : %d ",count++,v1,v2,c);
+			mincost = mincost + c;
+		}
+		cost[v1][v2] = cost[v2][v1] = 999;
+	}    
+	printf("\n Minimum cost=%d",mincost);  
+	return 0;
+} 
